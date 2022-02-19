960afdd3e817f8af82828316e7bde039a73601fd
Pervez alam <pervezalam777@gmail.com>
2022-02-19 20:59:46 +0530

    loop and task

---
+++ loopAndCondition/loop.js
@@ -0,0 +1,53 @@
+// for, for in, for of, while, do while
+// continue and break;
+//for(initialize ; condition ; increment)
+// i++ -> i = i + 1;
+for(let i = 0; i < 10; i++){
+  console.log('i value', i, i*2);
+}
+
+const array = ['Raj', 'Arif', 'Ram', 'Pervez', 'Narender']
+for (let index = array.length - 1; index >= 0 ; index--) {
+  const element = array[index];
+  console.log('Name is :', element)
+}
+
+const colors = { RED:'#FF0000', GREEN:'#00FF00', BLUE: '#0000FF', WHITE:'#FFFFFF'}
+for(let colorKey in colors){
+  console.log("Color key is ", colorKey, "color value is", colors[colorKey])
+}
+
+for(let name of array) {
+  console.log('Name is', name);
+}
+
+// while(condition) { }
+let whileIndex = 0;
+while(whileIndex < 10) {
+  whileIndex++;
+  console.log('while index now ', whileIndex);
+}
+
+let doWhileIndex = 10
+do {
+  console.log('Do while index', doWhileIndex)
+} while(doWhileIndex < 10)
+
+let doWhileIndex1 = 1
+do {
+  console.log('Do while index second', doWhileIndex1)
+  doWhileIndex1++;
+} while(doWhileIndex1 < 10)
+
+
+const nameArray = ['Raj', 'Arif', 'Ram', 'Pervez', 'Narender', 'Vijay'];
+for (let index = 0; index < nameArray.length; index++) {
+  const name = nameArray[index];
+  if(name === 'Narender') {
+    break;
+  }
+  if(name === 'Pervez') {
+    continue;
+  }
+  console.log('Name of persons', name);
+}
---
+++ task/task1.md
@@ -0,0 +1,6 @@
+- [] Create an array with 10 object of persons
+- [] Each object should have name, email, phone number, gender
+- [] There should be 4 male and 6 female
+
+- [] Write a code to arrange all male objects before female object and print
+- [] Write a code to arrange a combination like [[male, female], [male, female], [female, female]]
