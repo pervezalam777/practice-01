81490d3f4b6284862a96373e8c5c0a85a4886120
Pervez alam <pervezalam777@gmail.com>
2023-07-23 11:00:41 +0530

    My reduce function created also solution provided for exercise 01

--- variables/array/array_reduce_ex-01.js
+++ variables/array/array_reduce_ex-01.js
@@ -8,7 +8,46 @@ const array = [
   {type:"pant", price:3000, quantity:5, size:['36', '38'], category:'mens'},
 ]
 
+const array2 = [
+  {type:"shoe", price:1000, quantity:5, size:['9', '10'], category:'mens'},
+  {type:"shoe", price:1000, quantity:5, size:['7', '6'], category:'women'},
+  {type:"lipstick", price:5000, quantity:5, size:[], category:'women', colors:['red', 'pink']},
+  {type:"pant", price:3000, quantity:5, size:['36', '38'], category:'mens'},
+]
 
+const result = array.reduce((preResult, currentObject) => {
+  //preResult[currentObject.category].push(currentObject);
+  if(currentObject.category === 'mens'){
+    preResult.mens.push(currentObject);
+  } else {
+    preResult.women.push(currentObject);
+  }
+  return preResult; // must
+}, { mens:[], women: []})
+
+//console.log(result);
+
+Array.prototype.myReduce = function(callbackFn, initialValue) {
+  let output = initialValue;
+  const len = this.length; // this means array which its referring to.
+  for (let index = 0; index < len; index++) {
+    const element = this[index];
+    output = callbackFn(output, element)
+  }
+  return output;
+}
+
+const myResult = array2.myReduce((preResult, currentObject) => {
+  //preResult[currentObject.category].push(currentObject);
+  if(currentObject.category === 'mens'){
+    preResult.mens.push(currentObject);
+  } else {
+    preResult.women.push(currentObject);
+  }
+  return preResult; // must
+}, { mens:[], women: []});
+
+console.log('my result: ',myResult);
 /**
  * output one:
  * 
